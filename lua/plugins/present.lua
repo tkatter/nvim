ac66f427680462fc5e38a4cc60517d49cdb1ecb3
tkatter <tjkatter@gmail.com>
2025-06-26 20:01:53 -0500

    added mac config for `present` plugin

--- lua/plugins/present.lua
+++ lua/plugins/present.lua
@@ -1,5 +1,9 @@
 return {
+    -- LINUX PATH
     dir = '/home/thomas/plugins/present.nvim',
+
+    -- MAC PATH
+    -- dir = '/Users/thomaskatter/Plugins/nvim-plugins/present.nvim',
     config = function()
         require('present').setup {
             -- Set up the present.nvim plugin with default options
