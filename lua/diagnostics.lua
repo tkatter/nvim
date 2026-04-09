28a98f161587e92366b24c4663db7b1ff4b2c517
Thomas Katter <tjkatter@gmail.com>
2026-04-09 04:14:33 -0500

    commented out diagnostic example handler

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,17 +1,17 @@
 -- It's good practice to namespace custom handlers to avoid collisions
-vim.diagnostic.handlers["my/notify"] = {
-  show = function(namespace, bufnr, diagnostics, opts)
-    -- In our example, the opts table has a "log_level" option
-    local level = opts["my/notify"].log_level
-
-    local name = vim.diagnostic.get_namespace(namespace).name
-    local msg = string.format("%d diagnostics in buffer %d from %s",
-                              #diagnostics,
-                              bufnr,
-                              name)
-    vim.notify(msg, level)
-  end,
-}
+-- vim.diagnostic.handlers["my/notify"] = {
+--   show = function(namespace, bufnr, diagnostics, opts)
+--     -- In our example, the opts table has a "log_level" option
+--     local level = opts["my/notify"].log_level
+--
+--     local name = vim.diagnostic.get_namespace(namespace).name
+--     local msg = string.format("%d diagnostics in buffer %d from %s",
+--                               #diagnostics,
+--                               bufnr,
+--                               name)
+--     vim.notify(msg, level)
+--   end,
+-- }
 
 -- diagnostic-on-jump-example
 -- diagnostic-loclist-example
