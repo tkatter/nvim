7096f0b3243bb762da91dbd914230e45888d3022
Thomas Katter <tjkatter@gmail.com>
2026-04-09 16:43:44 -0500

    update lua_ls lsp config

--- lua/lsp/lua_ls.lua
+++ lua/lsp/lua_ls.lua
@@ -1,5 +1,6 @@
 vim.lsp.config('lua_ls', {
   on_init = function(client)
+    -- use .luarc.json(ck if present
     if client.workspace_folders then
       local path = client.workspace_folders[1].name
       if path ~= vim.fn.stdpath 'config'
@@ -11,14 +12,12 @@ vim.lsp.config('lua_ls', {
     end
 
     local lua_libs = vim.api.nvim_get_runtime_file("", true)
-    local os = require('luv').os_uname()
-
-    if os['sysname'] == 'Linux' then
+    if vim.uv.os_uname()['sysname'] == 'Linux' then
       lua_libs = vim.tbl_extend('keep',
         { '/opt/lua-language-server/meta/3rd/luv/library' }, lua_libs)
-    end
-    if os['sysname'] == 'Darwin' then
-      lua_libs = vim.tbl_extend('keep', { '${3rd}/luv/library' }, lua_libs)
+      else
+      lua_libs = vim.tbl_extend('keep',
+        { '${3rd}/luv/library' }, lua_libs)
     end
 
     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
@@ -47,7 +46,7 @@ vim.lsp.config('lua_ls', {
     '--metapath',
     '/home/thomas/.cache/lua_ls/meta/',
   },
-  root_markers = { 'stylua.toml', '.git' },
+  root_markers = { {'.luarc.json', '.luarc.jsonc'}, 'stylua.toml', '.git' },
   filetypes= { 'lua' },
   settings = {
       Lua = {},
